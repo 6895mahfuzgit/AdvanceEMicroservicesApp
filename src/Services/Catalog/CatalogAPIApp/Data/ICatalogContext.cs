ce344715fc3d5738e4ea2a1bcde78a3a72878e60
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-05 19:26:29 +0600

    Create ICatalogContext.cs

---
+++ src/Services/Catalog/CatalogAPIApp/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using CatalogAPIApp.Entities;
+using MongoDB.Driver;
+
+namespace CatalogAPIApp.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
