208270995ec15cde0d8c1542801bcaa168af566c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-05 19:28:33 +0600

    created catalog controller and fetch all product repo

---
+++ src/Services/Catalog/CatalogAPIApp/Controllers/CatalogController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CatalogAPIApp.Entities;
+using CatalogAPIApp.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace CatalogAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            this._productRepository = productRepository;
+            this.logger = logger;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+
+    }
+}
