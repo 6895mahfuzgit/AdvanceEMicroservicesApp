8762c6d4ac26d6ecfc1c10daef38a2b5a75387ee
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-12 18:15:47 +0600

    Installed Microsoft.Extensions.Caching.StackExchangeRedis Package

---
+++ src/Services/Basket/BasketAPIApp/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BasketAPIApp.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
