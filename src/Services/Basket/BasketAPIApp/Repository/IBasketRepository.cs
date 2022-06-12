fb6e431bd6fad2450e034b07205fc62ce890d688
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-12 19:31:54 +0600

    created IBasket Repo and implimented it

---
+++ src/Services/Basket/BasketAPIApp/Repository/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using BasketAPIApp.Entities;
+
+namespace BasketAPIApp.Repository
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+    }
+}
