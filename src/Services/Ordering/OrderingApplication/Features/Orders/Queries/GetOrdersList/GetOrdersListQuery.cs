965694a7b4dae8b8e2a4314c303aa5ea7d8f6eac
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-25 20:10:14 +0600

    implimented order Query

---
+++ src/Services/Ordering/OrderingApplication/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace OrderingApplication.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+
+
+    }
+}
