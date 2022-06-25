aa92c39e87529f1f47306eb3fbc21c28feaac42d
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-25 21:25:52 +0600

    created UpdateOrderCommand and it's handler with validation.

---
+++ src/Services/Ordering/OrderingApplication/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OrderingApplication.Contracts.Presistence;
+using OrderingDomain.Entities;
+
+namespace OrderingApplication.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (orderToUpdate == null)
+            {
+                _logger.LogError("Order not exists on Database.");
+            }
+
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is updated successfully.");
+
+            return Unit.Value;
+        }
+    }
+}
