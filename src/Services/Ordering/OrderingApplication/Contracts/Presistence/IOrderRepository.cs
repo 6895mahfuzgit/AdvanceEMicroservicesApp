dc4eac3d122f55a0496587e361c9932b3d958ade
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-25 15:35:07 +0600

    created base Repor and Order repo interface

---
+++ src/Services/Ordering/OrderingApplication/Contracts/Presistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrderingDomain.Entities;
+
+namespace OrderingApplication.Contracts.Presistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
