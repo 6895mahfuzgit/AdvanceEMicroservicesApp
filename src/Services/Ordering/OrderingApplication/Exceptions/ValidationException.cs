fc30ef59a0b88cc387f769dc390374ffe3984145
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 00:12:40 +0600

    Create ValidationException.cs

---
+++ src/Services/Ordering/OrderingApplication/Exceptions/ValidationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
+namespace OrderingApplication.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("One or More validation failures have accurred.")
+        {
+            Erros = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Erros = failures
+                  .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                  .ToDictionary(failureGrouped => failureGrouped.Key, failureGrouped => failureGrouped.ToArray());
+        }
+
+        public Dictionary<string, string[]> Erros { get; }
+
+    }
+}
