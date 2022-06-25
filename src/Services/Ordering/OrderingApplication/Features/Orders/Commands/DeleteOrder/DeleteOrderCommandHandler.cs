f77448f289566758ab7f199877df690c2dbb0a8b
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 00:16:43 +0600

    created DeleteOrderCommand and it's handler with validation.

---
+++ src/Services/Ordering/OrderingApplication/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OrderingApplication.Contracts.Presistence;
+using OrderingApplication.Exceptions;
+using OrderingDomain.Entities;
+
+namespace OrderingApplication.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+
+
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+
+            var orderFromDb = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (orderFromDb == null)
+            {
+                _logger.LogError("Order does not exists.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(orderFromDb);
+
+            _logger.LogInformation("Order deleted successfully.");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/OrderingApplication/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace OrderingApplication.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                   .NotEmpty()
+                    .WithMessage("{Id} can't be empty.")
+                   .NotNull()
+                   .WithMessage("{Id} can't be null.")
+                   .GreaterThan(0)
+                   .WithMessage("{Id} should be greater than 0.");
+        }
+    }
+}
