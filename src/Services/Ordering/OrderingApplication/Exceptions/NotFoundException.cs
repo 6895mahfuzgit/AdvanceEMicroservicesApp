9a3964dfbd80c83f84e10cc78e19a4c500e96b06
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 00:12:36 +0600

    Create NotFoundException.cs

---
+++ src/Services/Ordering/OrderingApplication/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OrderingApplication.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+
+        public NotFoundException(string name, Object key)
+            : base($" Entity \" {name} \" ({key})  was not found")
+        {
+
+        }
+    }
+}
