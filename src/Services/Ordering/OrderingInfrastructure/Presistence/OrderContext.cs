c5d3d79b782c399d12f710aebeb02e7149271d4c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 14:12:25 +0600

    created OrderContext

---
+++ src/Services/Ordering/OrderingInfrastructure/Presistence/OrderContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderingDomain.Common;
+using OrderingDomain.Entities;
+
+namespace OrderingInfrastructure.Presistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var entity in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        entity.Entity.CreatedDate = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entity.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
