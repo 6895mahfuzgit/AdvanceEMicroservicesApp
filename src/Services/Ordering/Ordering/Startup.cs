8c19ae6ba62d7b3ddea701280d930943e8734785
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 23:44:35 +0600

    registered 'AddApplicationServices' and 'AddInfrastructureSerices' in OrderAPI project

--- src/Services/Ordering/Ordering/Startup.cs
+++ src/Services/Ordering/Ordering/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OrderingApplication;
+using OrderingInfrastructure;
 
 namespace Ordering
 {
@@ -25,6 +27,8 @@ namespace Ordering
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
+            services.AddInfrastructureSerices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
--- src/Services/Ordering/OrderingInfrastructure/Presistence/OrderContextSeed.cs
+++ src/Services/Ordering/OrderingInfrastructure/Presistence/OrderContextSeed.cs
@@ -22,7 +22,7 @@ namespace OrderingInfrastructure.Presistence
         {
             return new List<Order>
             {
-                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLink = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
             };
         }
     }
