77d14beb0fe17ee2cca62506eb39e0665b378127
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 20:34:13 +0600

    implimented base repo and order repo. added order seed data class

---
+++ src/Services/Ordering/OrderingInfrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderingApplication.Contracts.Presistence;
+using OrderingDomain.Entities;
+using OrderingInfrastructure.Presistence;
+
+namespace OrderingInfrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _context.Orders.Where(o => o.UserName == userName).ToListAsync();
+            return orderList;
+        }
+
+    }
+}
