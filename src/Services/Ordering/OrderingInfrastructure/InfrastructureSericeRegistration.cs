9aa365cc7ee22aa167f1234ae289c4f63600b5ea
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-06-26 23:30:16 +0600

    created InfrastructureSericeRegistration

---
+++ src/Services/Ordering/OrderingInfrastructure/InfrastructureSericeRegistration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderingApplication.Contracts.Infrastructure;
+using OrderingApplication.Contracts.Presistence;
+using OrderingApplication.Models;
+using OrderingInfrastructure.Mail;
+using OrderingInfrastructure.Presistence;
+using OrderingInfrastructure.Repositories;
+
+namespace OrderingInfrastructure
+{
+    public static class InfrastructureSericeRegistration
+    {
+
+        public static IServiceCollection AddInfrastructureSerices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<OrderContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));
+            });
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c=>configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+
+    }
+}
