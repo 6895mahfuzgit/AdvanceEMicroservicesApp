07e17b1d7d19537120b438936ecdd9bdc71199e5
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-07-02 20:28:57 +0600

    created hosting helper  call and migrate and seed data for Order

---
+++ src/Services/Ordering/Ordering/Extensions/HostExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OrderingAPIApp.HostExtensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrationDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder, int? retry = 0) where TContext : DbContext
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetRequiredService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migration database associated with context {DbContextName}");
+                    InvockeSeeder(seeder, context, services);
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrationDatabase<TContext>(host, seeder, retryForAvailability);
+                    }
+                }
+            }
+            return host;
+        }
+
+        private static void InvockeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
--- src/Services/Ordering/Ordering/Program.cs
+++ src/Services/Ordering/Ordering/Program.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OrderingAPIApp.HostExtensions;
+using OrderingInfrastructure.Presistence;
 
 namespace Ordering
 {
@@ -13,7 +16,15 @@ namespace Ordering
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrationDatabase<OrderContext>((context, services) =>
+            {
+                var logger = services.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed
+                   .SeedAsync(context, logger)
+                   .Wait();
+            });
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
