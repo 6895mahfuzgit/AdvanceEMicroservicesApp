1bada85eb9d65273d68c73a7331962ae10fddeba
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-07-03 12:59:47 +0600

    Create IntregrationBaseEvent.cs

---
+++ src/BuildingBlocks/EventBusMessages/Events/IntregrationBaseEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventBusMessages.Events
+{
+    public class IntregrationBaseEvent
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public IntregrationBaseEvent()
+        {
+            Id = new Guid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntregrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+    }
+}
