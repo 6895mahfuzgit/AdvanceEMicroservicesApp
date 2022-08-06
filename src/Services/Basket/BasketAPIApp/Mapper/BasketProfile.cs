2aedbc3ac66eefb8a51b3e62cc066b35af6a8c79
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-08-06 14:05:09 +0600

    Create BasketProfile.cs

---
+++ src/Services/Basket/BasketAPIApp/Mapper/BasketProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BasketAPIApp.Entities;
+using EventBusMessages.Events;
+
+namespace BasketAPIApp.Mapper
+{
+    public class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>()
+                .ReverseMap();
+        }
+    }
+}
